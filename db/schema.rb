cc24b0be0702f04c09a8bec4a59f67e24437f80a
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-02 20:05:48 +0700

    Add Like and Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_125835) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_130512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "follow_requests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,6 +30,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_125835) do
     t.integer "sender_id"
     t.integer "receiver_id"
     t.boolean "accepted", default: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -44,5 +63,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_02_125835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
