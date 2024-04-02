135aa52a0013debe5d9a2eac755eb370e1835699
thaibhoang <hoangthai.ftu53@gmail.com>
2024-04-02 17:41:40 +0700

    Add function: User can request to follow others

---
+++ app/controllers/follow_requests_controller.rb
@@ -0,0 +1,67 @@
+class FollowRequestsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @sent_follow_requests = FollowRequest.where(sender_id: current_user.id, accepted: false).includes(:receiver)
+    @received_follow_requests = FollowRequest.where(receiver_id: current_user.id, accepted: false).includes(:sender)
+  end
+
+  def new
+    @request = FollowRequest.new
+  end
+
+  def create
+    receiver = User.find_by(email: params[:follow_request][:receiver_email])
+    if receiver.nil?
+      @error_message = "User with email '#{params[:follow_request][:receiver_email]}' not found"
+      @request = FollowRequest.new
+      render :new, status: :unprocessable_entity
+    elsif request_existed(receiver.id, current_user.id)
+      @error_message = "Request already existed"
+      @request = FollowRequest.new
+      render :new, status: :unprocessable_entity
+    else
+      @request = current_user.sent_follow_requests.build(receiver_id: receiver.id)
+
+      respond_to do |format|
+        if @request.save
+          format.turbo_stream 
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  def destroy
+    @request = FollowRequest.find(params[:id])
+    @request.destroy
+
+    flash[:notice] = "Removed follow request"
+    redirect_to follow_requests_path
+  end
+
+  def accept
+    @request = FollowRequest.find(params[:id])
+    if current_user == @request.receiver
+      @request.accepted = true
+      if @request.save
+        flash[:notice] = 'Request accepted!'
+      else
+        flash[:error] = "Failed to accept request."
+      end
+    end
+    redirect_to follow_requests_path
+  end
+
+  private
+
+  def follow_request_params
+    params.require(:follow_request).permit(:sender_id, :receiver_id, :receiver_email)
+  end
+
+  def request_existed(receiver_id, sender_id)
+    request = FollowRequest.find_by(receiver_id: receiver_id, sender_id: sender_id)
+    return !request.nil?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :follow_requests do
+    put "accept", on: :member
+  end
+
 end
